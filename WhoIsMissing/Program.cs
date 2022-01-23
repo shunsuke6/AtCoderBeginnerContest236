be838c08396ffca230227da906853364369adc25
shunsuke6 <snsk.syk.gtobzz@gmail.com>
2022-01-24 01:35:54 +0900

    first commit

---
+++ WhoIsMissing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace WhoIsMissing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long N = long.Parse(Console.ReadLine());
+            long[] card = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            var sumCard = N * (1 + N) * 2;
+            Console.WriteLine(sumCard - card.Sum());
+
+
+        }
+    }
+}
---
+++ chukodai/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace chukodai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S =Console.ReadLine().ToArray();
+            int[] index = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var  a = S[index[0]-1];
+            var b  = S[index[1]-1];
+            
+            S[index[0]-1] = b;
+            S[index[1]-1] = a;
+           
+            Console.WriteLine(S);
+        }
+    }
+}
